50f32072341ff1bedb8a409a5aa24c1c0ae2d7b4
eng Abubakr Mahdi <engabubakrmahdi@hotmail.com>
2022-03-16 08:54:50 +0200

    Added New Feature.

--- Demo Application/MainWindow.Designer.cs
+++ Demo Application/MainWindow.Designer.cs
@@ -35,6 +35,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -106,11 +107,21 @@
             this.button2.Text = "Feature!!!";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(110, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 27);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Feature!!!";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
@@ -135,6 +146,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
